9e448248b2c1e278d5ae0b317225ce80e83e3696
Jasmine <jasmineamber.ja@gmail.com>
2023-02-22 21:41:49 +0000

    adding image to about section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -15,6 +15,8 @@ function About() {
                 <p className="Intro">I came to software development from a marketing background after completing the Manchester Codes Software Engineering Bootcamp and have been working as a Software Engineer professionally since June 2020. 
                 During this time I have also completed my Level 4 Software Engineering Apprenticeship and had the privilege of working on several different projects professionally ranging from JavaScript and React to Infrastructure, .NET and iOS Development. 
                 I am currently working as an iOS Engineer at Footasylum.</p>
+                <br />
+                <img className="Jasmine" src={require('../images/jasmine-picture.png')} alt="Jasmine"></img>
             </div>
         </div>
     )
