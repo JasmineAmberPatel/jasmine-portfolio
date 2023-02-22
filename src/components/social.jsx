2ec156a83c6202631632178edaaa727cb73aa5b0
Jasmine <jasmineamber.ja@gmail.com>
2023-02-22 13:39:20 +0000

    increase button sizes for accessibility

--- src/components/social.jsx
+++ src/components/social.jsx
@@ -9,10 +9,10 @@ function Social() {
             <a className="btn btn-dark cv" href="https://drive.google.com/file/d/1UKlpOkBVneVc5_WlP2MSvIUu9yXdaS49/view?usp=sharing" target="_blank" rel="noopener noreferrer">Download CV</a>
             <br />
             <div className="social-icons">
-                <SocialIcon url="https://www.linkedin.com/in/jasmine-amber-patel-b04956a1/" network="linkedin" target="_blank" style={{ height: 25, width: 25 }} bgColor="#272727" fgColor="#ffffff" />
-                <SocialIcon url="https://twitter.com/CodesJamber" network="twitter" target="_blank" style={{ height: 25, width: 25, margin: 4 }} bgColor="#272727" fgColor="#ffffff" />
-                <SocialIcon url="https://github.com/JasmineAmberPatel" network="github" target="_blank" style={{ height: 25, width: 25 }} bgColor="#272727" fgColor="#ffffff" />
-                <SocialIcon url="https://www.instagram.com/jamber_ceramics/" network="instagram" target="_blank" style={{ height: 25, width: 25, margin: 4 }} bgColor="#272727" fgColor="#ffffff" />
+                <SocialIcon url="https://www.linkedin.com/in/jasmine-amber-patel-b04956a1/" network="linkedin" target="_blank" style={{ height: 35, width: 35 }} bgColor="#272727" fgColor="#ffffff" />
+                <SocialIcon url="https://twitter.com/CodesJamber" network="twitter" target="_blank" style={{ height: 35, width: 35, margin: 10 }} bgColor="#272727" fgColor="#ffffff" />
+                <SocialIcon url="https://github.com/JasmineAmberPatel" network="github" target="_blank" style={{ height: 35, width: 35 }} bgColor="#272727" fgColor="#ffffff" />
+                <SocialIcon url="https://www.instagram.com/jamber_ceramics/" network="instagram" target="_blank" style={{ height: 35, width: 35, margin: 10 }} bgColor="#272727" fgColor="#ffffff" />
             </div>
             <p>© 2023 Jasmine Patel</p>
         </div>
