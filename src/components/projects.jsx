d668a87a23c9fd0daa1365a8dd7055685ab9c36a
Jasmine <jasmineamber.ja@gmail.com>
2023-10-29 18:03:10 +0000

    Remove unnecessary classes

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -7,9 +7,9 @@ function Projects() {
         <div className="Projects" id="projects">
             <h2 className="Subtitle project-title">Projects</h2>
             <div className="Projects_container">
-            <div className="Projects_box flickr">
+                <div className="Projects_box flickr">
                     <img className="Projects_image" src={require('../images/photo-finder.png')} alt="flickr finder app"></img>
-                    <div className="Project-info gq-info">
+                    <div className="Project-info">
                         <h2 className="Subtitle">Photo Finder App</h2>
                         <p className="Paragraph">An iOS app built with Swift and SwiftUI. It leverages the MVVM (Model-View-ViewModel) architectural pattern and integrates with the Flickr API to search and display photos by keyword and by user.
                         </p>
@@ -20,7 +20,7 @@ function Projects() {
                 </div>
                 <div className="Projects_box community">
                     <img className="Projects_image" src={require('../images/community-events-1.png')} alt="community events app"></img>
-                    <div className="Project-info gq-info">
+                    <div className="Project-info">
                         <h2 className="Subtitle">Community Events App</h2>
                         <p className="Paragraph">An iOS app built with Swift and SwiftUI. The app has an interactive map which shows all the community events happening in a given location. Once a community event is clicked on, on the map, it will display more detailed information about the event such as the location, date, time, cost and a brief description and picture. The app consumes a .NET minimal API which is linked to a MongoDB database.
                         </p>
@@ -32,7 +32,7 @@ function Projects() {
                 </div>
                 <div className="Projects_box spotify">
                     <img className="Projects_image" src={require('../images/spotify-mosaic.png')} alt="spotify mosaic"></img>
-                    <div className="Project-info gq-info">
+                    <div className="Project-info">
                         <h2 className="Subtitle">Music Mosaic</h2>
                         <p className="Paragraph">A React application which uses the Spotify API to get the album art of a users 50 most recently listened to songs on Spotify and turn it into a mosaic. The backend is an auth server which validates a users spotify account.
                         </p>
@@ -44,7 +44,7 @@ function Projects() {
                 </div>
                 <div className="Projects_box snacky">
                     <img className="Projects_image" src={require('../images/snacky.png')} alt="snacky snake game"></img>
-                    <div className="Project-info gq-info">
+                    <div className="Project-info">
                         <h2 className="Subtitle">Snacky Snake</h2>
                         <p className="Paragraph">A classic snake game created using vanilla JavaScript and styled with CSS. All graphics were also created by me using adobe illustrator.
                         </p>
@@ -56,7 +56,7 @@ function Projects() {
                 </div>
                 <div className="Projects_box golden">
                     <img className="Projects_image" src={require('../images/golden-shoe-01.png')} alt="golden shoe"></img>
-                    <div className="Project-info gq-info">
+                    <div className="Project-info">
                         <h2 className="Subtitle">Golden Shoe</h2>
                         <p className="Paragraph">A prototype of an ecommerce website created with React and styled with Bootstrap and Sass.
                             The prototype allows you to filter shoes by price, size and popularity. It also allows you to add and remove shoes from the basket.
@@ -67,8 +67,8 @@ function Projects() {
                     </div>
                 </div>
                 <div className="Projects_box">
-                    <img className="Projects_image gq" src={require('../images/GQ-01.png')} alt="green queens"></img>
-                    <div className="Project-info gq-info">
+                    <img className="Projects_image" src={require('../images/GQ-01.png')} alt="green queens"></img>
+                    <div className="Project-info">
                         <h2 className="Subtitle">Green Queens</h2>
                         <p className="Paragraph">A website which highlights the impact of the fast fashion industry on the environment.
                             This was a group project for which I took on the role of team leader.
@@ -81,8 +81,8 @@ function Projects() {
                     </div>
                 </div>
                 <div className="Projects_box">
-                    <img className="Projects_image queens" src={require('../images/queens-01.png')} alt="quotes from queens"></img>
-                    <div className="Project-info queens-info">
+                    <img className="Projects_image" src={require('../images/queens-01.png')} alt="quotes from queens"></img>
+                    <div className="Project-info">
                         <h2 className="Subtitle">Quotes From Queens</h2>
                         <p className="Paragraph">A React application, which consumes an Express API containing quotes from inspirational female artists and celebrities, stored in an MongoDB database.</p>
                         <div className="buttons">
@@ -93,7 +93,7 @@ function Projects() {
                 </div>
                 <div className="Projects_box music">
                     <img className="Projects_image" src={require('../images/music-01.png')} alt="music library api"></img>
-                    <div className="Project-info gq-info">
+                    <div className="Project-info">
                         <h2 className="Subtitle">Music Library API</h2>
                         <p className="Paragraph">This CRUD REST API stores information about artists, albums and songs and interacts with a MongoDB database using Express.
                             I used Mongoose to write the MongoDB validation in the form of schemas and models. I also used Test Driven Development and wrote tests using Mocha and Chai.
@@ -105,11 +105,11 @@ function Projects() {
                 </div>
                 <div className="Projects_box  weather">
                     <img className="Projects_image" src={require('../images/weather-01.png')} alt="weather app"></img>
-                    <div className="Project-info weather-info">
+                    <div className="Project-info">
                         <h2 className="Subtitle">Weather Forecast</h2>
                         <p className="Paragraph">A 5-day weather forecasting app built with React consuming an open weather API. You can search to see the weather at any UK destination. Styled using CSS.</p>
                         <div className="buttons">
-                            <a className="btn" href="https://github.com/JasmineAmberPatel/weather-app" target="_blank" rel="noopener noreferrer">View Source</a>
+                            <a className="btn" href="https://github.com/JasmineAmberPatel/weather-app" target="_blank" rel="noopener noreferrer">View Code</a>
                         </div>
                     </div>
                 </div>
